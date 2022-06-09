74bca9288f1ae9d225376b8b8776edb64dab29e0
DESKTOP-GAHLO8A\Morgan <rusllann97@gmail.com>
2022-06-09 22:40:28 +0500

    Добавил парсер протокола AlphaWave

---
+++ BluetoothWpf/Models/AlphaWavePacket.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BluetoothWpf.Models
+{
+    public class AlphaWavePacket
+    {
+        private byte _attention;
+        private byte _meditation;
+        private byte _crc8;
+        private UInt32 _packetNumber;
+
+        public byte Attention
+        {
+            get => _attention;
+            set => _attention = value;
+        }
+
+        public byte Meditation
+        {
+            get => _meditation;
+            set => _meditation = value;
+        }
+
+        public byte Crc8
+        {
+            get => _crc8;
+            set => _crc8 = value;
+        }
+
+        public uint PacketNumber
+        {
+            get => _packetNumber;
+            set => _packetNumber = value;
+        }
+
+        public AlphaWavePacket(byte attention, byte meditation, uint packetNumber = 0)
+        {
+            _attention = attention;
+            _meditation = meditation;
+            _packetNumber = packetNumber;
+            _crc8 = (byte)(_attention ^ _meditation);
+        }
+
+        public bool IsCrcOk(byte receivedCrc)
+        {
+            return _crc8 == receivedCrc;
+        }
+        
+    }
+}
---
+++ BluetoothWpf/Models/AlphaWaveProtocolParser.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BluetoothWpf.Models
+{
+    public class AlphaWaveProtocolParser
+    {
+        // Regex protcolPattern = "$$CRC8=%d;A=%d;M=%d;#%d;$$"
+        private const string _protcolPattern = 
+            @"\$\$CRC8=(?<crc8>\d+?);A=(?<attention>\d+?);M=(?<meditation>\d+?);#(?<packNumber>\d+?);$$";
+
+        public AlphaWavePacket ParsePacketFromString(string receivedPacket)
+        {
+            var matches = Regex.Matches(receivedPacket, _protcolPattern);
+            if (matches != null && matches.Count > 0)
+            {
+                var match = matches[0];
+                var crc8String = match.Groups["crc8"].Value;
+                var attentionString = match.Groups["attention"].Value;
+                var meditationString = match.Groups["meditation"].Value;
+
+                byte rxCrc8 = 0;
+                byte rxAttention = 0;
+                byte rxMeditation = 0;
+                
+                bool isValuesInPacketOk = byte.TryParse(crc8String, out rxCrc8) &&
+                                          byte.TryParse(attentionString, out rxAttention) &&
+                                          byte.TryParse(meditationString, out rxMeditation);
+
+                if (isValuesInPacketOk)
+                {
+                    AlphaWavePacket parsedPacket = new AlphaWavePacket(rxAttention, rxMeditation);
+                    if (parsedPacket.IsCrcOk(rxCrc8))
+                    {
+                        return parsedPacket;
+                    }
+                }
+            }
+            return null;
+        }
+        
+    }
+}

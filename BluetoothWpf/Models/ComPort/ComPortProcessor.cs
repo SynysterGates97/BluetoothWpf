0018028e5fb9c362662c5680cb079b07a4c01376
DESKTOP-GAHLO8A\Morgan <rusllann97@gmail.com>
2022-06-09 23:17:05 +0500

    Добавил в моделях свои папки для классов для работы с протколом AlphaWave, добавил класс для работы с COM-портами.

---
+++ BluetoothWpf/Models/ComPort/ComPortProcessor.cs
@@ -0,0 +1,24 @@
+﻿
+
+using RJCP.IO.Ports;
+
+namespace BluetoothWpf.Models.ComPort
+{
+    public class ComPortProcessor
+    {
+        private SerialPortStream _serialPortStream;
+
+        public ComPortProcessor(int comPortNumber)
+        {
+            var comPortString = $"COM{comPortNumber}";
+            _serialPortStream = new SerialPortStream(comPortString, 115200, 8, Parity.None, StopBits.One);
+            
+            _serialPortStream.DataReceived += SerialPortStreamOnDataReceived;
+        }
+
+        private void SerialPortStreamOnDataReceived(object? sender, SerialDataReceivedEventArgs e)
+        {
+            // e.EventType == SerialData.Chars
+        }
+    }
+}
